be91f5671fe40d563cfd518ca9885a2ea03506cc
Tyler Latimer <trlatimer95@gmail.com>
2022-05-03 21:13:25 -0700

    Created initial player states, added initial input mapping

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+{
+    public event Action jumpEvent;
+    public event Action dodgeEvent;
+
+    private Controls controls;
+
+    private void Start()
+    {
+        controls = new Controls();
+        controls.Player.SetCallbacks(this);
+
+        controls.Player.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Player.Disable();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        
+        jumpEvent?.Invoke();
+    }
+
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        dodgeEvent?.Invoke();
+    }
+}
