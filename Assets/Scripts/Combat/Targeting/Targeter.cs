cbef879a9014b094faa677b9b0c749dfc93367c3
Tyler Latimer <trlatimer95@gmail.com>
2022-05-14 22:44:41 -0700

    Implemented targeting movement and target preference

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -1,35 +1,82 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Targeter : MonoBehaviour
 {
+    [SerializeField] private CinemachineTargetGroup cinTargetGroup;
+
     private List<Target> targets = new List<Target> ();
+    private Camera mainCamera;
 
     public Target CurrentTarget { get; private set; }
 
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<Target>(out Target target))
-            targets.Add(target);
+        if (!other.TryGetComponent<Target>(out Target target)) return;
+        
+        targets.Add(target);
+        target.OnDestroyed += RemoveTarget;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.TryGetComponent<Target>(out Target target))
-            targets.Remove(target);
+        if (!other.TryGetComponent<Target>(out Target target)) return;
+        
+        RemoveTarget(target);
     }
 
     public bool SelectTarget()
     {
         if (targets.Count == 0) return false;
 
-        CurrentTarget = targets[0];
+        Target closestTarget = null;
+        float closestTargetDistance = Mathf.Infinity;
+
+        foreach (Target target in targets)
+        {
+            Vector2 viewPos = mainCamera.WorldToViewportPoint(target.transform.position);
+            if (viewPos.x < 0 || viewPos.x > 1 || viewPos.y < 0 || viewPos.y > 1) continue;
+
+            Vector2 toCenter = viewPos - new Vector2(0.5f, 0.5f);
+            if (toCenter.sqrMagnitude < closestTargetDistance)
+            {
+                closestTarget = target;
+                closestTargetDistance = toCenter.sqrMagnitude;
+            }
+        }
+
+        if (closestTarget == null) return false;
+
+        CurrentTarget = closestTarget;
+        cinTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
+
         return true;
     }
 
     public void Cancel()
     {
+        if (CurrentTarget == null) return;
+
+        cinTargetGroup.RemoveMember(CurrentTarget.transform);
         CurrentTarget = null;
+    }
+
+    private void RemoveTarget(Target target)
+    {
+        if (CurrentTarget == target)
+        {
+            cinTargetGroup.RemoveMember(CurrentTarget.transform);
+            CurrentTarget = null;
+        }
+
+        target.OnDestroyed -= RemoveTarget;
+        targets.Remove(target);
     }
 }
--- Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerTargetingState : PlayerBaseState
 {
     private readonly int TargetingBlendTreeHash = Animator.StringToHash("TargetingBlendTree");
+    private readonly int TargetingForwardHash = Animator.StringToHash("TargetingForward");
+    private readonly int TargetingRightHash = Animator.StringToHash("TargetingRight");
 
     public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine){}
 
@@ -16,7 +18,18 @@ public class PlayerTargetingState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        
+        if(stateMachine.Targeter.CurrentTarget == null)
+        {
+            stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+            return;
+        }
+
+        Vector3 movement = CalculateMovement();
+        Move(movement * stateMachine.TargetingLookMovementSpeed, deltaTime);
+
+        UpdateAnimator(deltaTime);
+
+        FaceTarget();
     }
 
 
@@ -29,5 +42,31 @@ public class PlayerTargetingState : PlayerBaseState
     {
         stateMachine.Targeter.Cancel();
         stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 movement = new Vector3();
+        movement += stateMachine.transform.right * stateMachine.InputReader.MovementValue.x;
+        movement += stateMachine.transform.forward * stateMachine.InputReader.MovementValue.y;
+        return movement;
+    }
+
+    private void UpdateAnimator(float deltaTime)
+    {
+        if (stateMachine.InputReader.MovementValue.y == 0)
+            stateMachine.Animator.SetFloat(TargetingForwardHash, 0, 0.1f, deltaTime);
+        else
+        {
+            float value = stateMachine.InputReader.MovementValue.y > 0 ? 1f : -1f;
+            stateMachine.Animator.SetFloat(TargetingForwardHash, value, 0.1f, deltaTime);
+        }
+        if (stateMachine.InputReader.MovementValue.x == 0)
+            stateMachine.Animator.SetFloat(TargetingRightHash, 0, 0.1f, deltaTime);
+        else
+        {
+            float value = stateMachine.InputReader.MovementValue.x > 0 ? 1f : -1f;
+            stateMachine.Animator.SetFloat(TargetingRightHash, value, 0.1f, deltaTime);
+        }
     }
 }
